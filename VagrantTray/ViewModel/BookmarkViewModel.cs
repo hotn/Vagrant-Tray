93f38f8791f4d709a0535fe3ff04edaf12350038
Mike Walton <mike@blueearth.net>
2014-06-17 13:34:58 -0500

    create simple view models for potential settings windows

---
+++ VagrantTray/ViewModel/BookmarkViewModel.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.IO;
+using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+using MikeWaltonWeb.VagrantTray.Model;
+
+namespace MikeWaltonWeb.VagrantTray.ViewModel
+{
+    /// <summary>
+    /// This class contains properties that a View can data bind to.
+    /// <para>
+    /// See http://www.galasoft.ch/mvvm
+    /// </para>
+    /// </summary>
+    public class BookmarkViewModel : ViewModelBase
+    {
+        private readonly Bookmark _bookmark;
+        private RelayCommand _saveCommand;
+        private RelayCommand _cancelCommand;
+
+        public string BookmarkName
+        {
+            get
+            {
+                if (IsInDesignMode)
+                {
+                    return "My Bookmark";
+                }
+
+                return _bookmark != null ?_bookmark.Name : String.Empty;
+            }
+            //set;
+        }
+
+        public string VagrantInstanceId
+        {
+            get
+            {
+                if (IsInDesignMode)
+                {
+                    return "1234abc";
+                }
+
+                return _bookmark != null ? _bookmark.VagrantInstance.Id : String.Empty;
+            }
+        }
+
+        public string VagrantInstanceName
+        {
+            get
+            {
+                if (IsInDesignMode)
+                {
+                    return "Vagrant box";
+                }
+
+                return _bookmark != null ? _bookmark.VagrantInstance.Name : String.Empty;
+            }
+        }
+
+        public string VagrantInstanceLocation
+        {
+            get
+            {
+                if (IsInDesignMode)
+                {
+                    return "C:/somefolder/vagrant";
+                }
+
+                return _bookmark != null ? _bookmark.VagrantInstance.Directory : String.Empty;
+            }
+        }
+
+        public bool IsPresent
+        {
+            get { return Directory.Exists(VagrantInstanceLocation); }
+        }
+
+        public RelayCommand SaveCommand
+        {
+            get
+            {
+                if (IsInDesignMode)
+                {
+                    return _defaultCommand;
+                }
+
+                return _saveCommand;
+            }
+            set
+            {
+                _saveCommand = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        public RelayCommand CancelCommand
+        {
+            get
+            {
+                if (IsInDesignMode)
+                {
+                    return _defaultCommand;
+                }
+
+                return _cancelCommand;
+            }
+            set
+            {
+                _cancelCommand = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        private readonly RelayCommand _defaultCommand = new RelayCommand(() => { }, () => true);
+
+        /// <summary>
+        /// Initializes a new instance of the BookmarkViewModel class.
+        /// </summary>
+        public BookmarkViewModel()
+        {
+        }
+
+        public BookmarkViewModel(Bookmark bookmark)
+        {
+            _bookmark = bookmark;
+        }
+    }
+}
---
+++ VagrantTray/ViewModel/SettingsViewModel.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+using MikeWaltonWeb.VagrantTray.Model;
+
+namespace MikeWaltonWeb.VagrantTray.ViewModel
+{
+    /// <summary>
+    /// This class contains properties that a View can data bind to.
+    /// <para>
+    /// See http://www.galasoft.ch/mvvm
+    /// </para>
+    /// </summary>
+    public class SettingsViewModel : ViewModelBase
+    {
+        private readonly ApplicationData _applicationData;
+
+        private bool _hasChanged;
+
+        public List<BookmarkViewModel> Bookmarks
+        {
+            get
+            {
+                return _applicationData.Bookmarks != null
+                    ? _applicationData.Bookmarks.Select(b => new BookmarkViewModel(b)).ToList()
+                    : new List<BookmarkViewModel>();
+            }
+        }
+
+        /// <summary>
+        /// This constructor is for design purposes only.
+        /// </summary>
+        public SettingsViewModel()
+        {
+            _applicationData = new ApplicationData();
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the SettingsViewModel class.
+        /// </summary>
+        public SettingsViewModel(ApplicationData applicationData)
+        {
+            _applicationData = applicationData;
+        }
+
+        private string _addBookmarkSelectedItem;
+
+        public string AddBookmarkSelectedItem
+        {
+            get
+            {
+                if (IsInDesignMode)
+                {
+                    return String.Empty;
+                }
+                return _addBookmarkSelectedItem;
+            }
+            set
+            {
+                _addBookmarkSelectedItem = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        
+
+        private RelayCommand _okCommand;
+
+        public RelayCommand OkCommand
+        {
+            get
+            {
+                if (IsInDesignMode)
+                {
+                    return _defaultRelayCommand;
+                }
+
+                return _okCommand;
+            }
+            set
+            {
+                _okCommand = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        private RelayCommand _cancelCommand;
+
+        public RelayCommand CancelCommand
+        {
+            get
+            {
+                if (IsInDesignMode)
+                {
+                    return _defaultRelayCommand;
+                }
+
+                return _cancelCommand;
+            }
+            set
+            {
+                _cancelCommand = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        private RelayCommand _closeCommand;
+
+        public RelayCommand CloseCommand
+        {
+            get
+            {
+                if (IsInDesignMode)
+                {
+                    return _defaultRelayCommand;
+                }
+
+                return _closeCommand;
+            }
+            set
+            {
+                _closeCommand = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        private readonly RelayCommand _defaultRelayCommand = new RelayCommand(() => { }, () => true);
+    }
+}
