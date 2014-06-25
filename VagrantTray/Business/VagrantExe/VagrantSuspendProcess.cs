23a17ae834dd899646a47ba7b222aca036181de2
Mike Walton <mike@blueearth.net>
2014-06-25 10:42:59 -0500

    add process for vagrant suspend

---
+++ VagrantTray/Business/VagrantExe/VagrantSuspendProcess.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using MikeWaltonWeb.VagrantTray.Model;
+
+namespace MikeWaltonWeb.VagrantTray.Business.VagrantExe
+{
+    public class VagrantSuspendProcess : VagrantProcess
+    {
+        public event EventHandler Success;
+
+        public VagrantSuspendProcess(VagrantInstance instance) : base(instance)
+        {
+            StartInfo.Arguments = "suspend";
+        }
+
+        protected override void OnExited(object sender, EventArgs eventArgs)
+        {
+            //TODO: announce errors
+
+            if (Success != null)
+            {
+                Success(this, EventArgs.Empty);
+            }
+        }
+    }
+}
