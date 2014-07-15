49ed299a29116a95fd2a75ab6d06eb4250c7e5f2
Mike Walton <mike@blueearth.net>
2014-07-15 15:00:35 -0500

    add comparison utility classes

---
+++ VagrantTray/Business/Utility/Comparers/BookmarkEqualityComparer.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using MikeWaltonWeb.VagrantTray.Model;
+
+namespace MikeWaltonWeb.VagrantTray.Business.Utility.Comparers
+{
+    public class BookmarkEqualityComparer : IEqualityComparer<Bookmark>
+    {
+        public bool Equals(Bookmark x, Bookmark y)
+        {
+            return x.Name == y.Name &&
+                   new VagrantInstanceEqualityComparer().Equals(x.VagrantInstance, y.VagrantInstance);
+        }
+
+        public int GetHashCode(Bookmark obj)
+        {
+            return obj.GetHashCode();
+        }
+    }
+}
---
+++ VagrantTray/Business/Utility/Comparers/VagrantInstanceEqualityComparer.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using MikeWaltonWeb.VagrantTray.Model;
+
+namespace MikeWaltonWeb.VagrantTray.Business.Utility.Comparers
+{
+    public class VagrantInstanceEqualityComparer : IEqualityComparer<VagrantInstance>
+    {
+        public bool Equals(VagrantInstance x, VagrantInstance y)
+        {
+            return x.Directory == y.Directory;
+        }
+
+        public int GetHashCode(VagrantInstance obj)
+        {
+            return obj.GetHashCode();
+        }
+    }
+}
